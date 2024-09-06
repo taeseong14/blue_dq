00023f447c3ac5bcf796428e5d56cb5bb7f13d23
taeseong14 <kwontaeseong14@gmail.com>
2024-09-06 23:52:39 +0900

    모모톡 수정

--- momotalk_auto.bat
+++ momotalk_auto.bat
@@ -5,7 +5,7 @@ cd ..
 
 :loop
 call :screencap
-node b_a/js/getPixelRGB.js 375 199 0
+node b_a/js/getPixelRGB.js 398 187 0
 for /f "tokens=*" %%a in (b_a\result.txt) do (
     if "%%a" == "240 68 0" (
         del b_a\screen.png b_a\result.txt
@@ -42,24 +42,40 @@ adb shell input tap 800 354
 echo student !stu!
 
 set /a loop2ed = 0
+echo.
 :loop2
+set /a loop2ed += 1
+echo [2K[1A[2Ktouching !loop2ed! ^(^>7 ^-^> err^)
+adb shell input swipe 1700 700 1700 500 300
+node b_a/js/w.js 1
 call :screencap
 node b_a/js/getPixelRGB.js 1749 846 0
 for /f "tokens=*" %%a in (b_a\result.txt) do (
     if "%%a" NEQ "255 237 240" (
+        if "%%a" == "119 222 255" (
+            echo err: goto loop 1
+            set /a stu -= 1
+            adb shell input tap 372 249
+            node b_a/js/w.js 1
+            adb shell input tap 372 399
+            node b_a/js/w.js 1
+            goto :loop1
+        )
         set /a i = 0
         del b_a\screen.png b_a\result.txt
         :loop3
-        set /a y = 900 - 80 * !i!
+        if !i! == 0 (
+            set /a y = 900
+        ) else (
+            set /a y = 900 - 120 - 80 * !i!
+        )
         set /a i += 1
         adb shell input tap 1700 !y!
         if !i! LSS 8 (
-            node b_a/js/w.js 200
+            node b_a/js/w.js 100
             goto :loop3
         )
-        set /a loop2ed += 1
-        echo touching !loop2ed!
-        if !loop2ed! GTR 10 (
+        if !loop2ed! GTR 7 (
             echo err: goto loop 1
             adb shell input tap 372 249
             node b_a/js/w.js 1
@@ -68,6 +84,7 @@ for /f "tokens=*" %%a in (b_a\result.txt) do (
             set /a stu -= 1
             goto :loop1
         )
+        node b_a/js/w.js 3
         goto :loop2
     )
 )
